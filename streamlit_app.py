15fcb467a61aae707f00251e459cbe4b548ba121
rezadivatama <rezadivatama@gmail.com>
2025-05-20 19:45:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("selamat datang di negeri wakanda")
-st.write( t.write(  ? 
-    "Let's "jalan jalan di Negeri Wakanda"
+st.write(
+    "jalan jalan di Negeri Wakanda"
 )

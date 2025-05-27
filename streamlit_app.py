f3f160e3eb92750e07f1dfccd1d6c15b1c047ac2
rezadivatama <rezadivatama@gmail.com>
2025-05-27 07:56:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,15 @@ st.write(
 ) 
 st.image("download.jpeg", width=200)
         
+
+
+st.title("hala madrid")
+st.header("madrid 15 ucl")
+angka = st.number_input("15:",value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah bilangan genap")
+else:
+    st.write (f"{angka} adalah bilangan ganjil")
+    
+
